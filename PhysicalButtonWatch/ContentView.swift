bb4200fe2e69c60fecc9c9ef39f74bff72941559
Ogijun2018 <o2030028@edu.cc.uec.ac.jp>
2020-12-18 03:21:12 +0900

    音量ボタンでの動作お試し

--- PhysicalButtonWatch/ContentView.swift
+++ PhysicalButtonWatch/ContentView.swift
@@ -6,18 +6,28 @@
 //
 
 import SwiftUI
+import AVFoundation
+import MediaPlayer
+import UIKit
 
 struct ContentView: View {
     @ObservedObject var stopWatchManager = StopWatchManager()
     
     var body: some View {
         VStack{
+            // ボリュームスライダー
+            VolumeSlider()
+               .frame(height: 40)
+               .padding(.horizontal)
             // タイムの表示
             Text(String(format: "%.2f", stopWatchManager.secondsElapsed))
                 .font(.custom("Avenir", size: 40))
                 .padding(.bottom, 50)
             if stopWatchManager.mode == .stopped {
-                Button(action: {self.stopWatchManager.start()}) {
+                Button(action: {
+                        self.stopWatchManager.start()
+                    self.stopWatchManager.plusAction()
+                }) {
                     TimerButton(label: "Start", buttonColor: .blue)
                 }
             }
@@ -41,7 +51,7 @@ struct ContentView: View {
                     }
                 }
             }
-        }
+        }.onAppear{print("表示された")}
     }
 }
 
@@ -49,6 +59,14 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct VolumeSlider: UIViewRepresentable {
+   func makeUIView(context: Context) -> MPVolumeView {
+      MPVolumeView(frame: .zero)
+   }
+
+   func updateUIView(_ view: MPVolumeView, context: Context) {}
 }
 
 struct TimerButton: View {
--- PhysicalButtonWatch/StopWatchManager.swift
+++ PhysicalButtonWatch/StopWatchManager.swift
@@ -6,17 +6,20 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 class StopWatchManager: ObservableObject {
     // ウォッチを開始してから経過した時間の変数
     @Published var secondsElapsed = 0.0
     // ウォッチの現在状態
     @Published var mode: stopWatchMode = .stopped
+
     // Documentを参照
     var timer = Timer()
     
     func start() {
         mode = .running
+        print("press start")
         timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true){_ in
             self.secondsElapsed += 0.01
         }
@@ -31,6 +34,39 @@ class StopWatchManager: ObservableObject {
     func pause() {
         timer.invalidate()
         mode = .paused
+    }
+    
+    var volumeValue : Float = 1.0
+    
+    func plusAction(){
+        print("plus pressed.")
+    }
+    
+    func minusAction(){
+        print("minus pressed.")
+    }
+    
+    @objc func volumeChanged(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            if let volumeChangeType = userInfo["AVSystemController_AudioVolumeChangeReasonNotificationParameter"] as? String {
+                if volumeChangeType == "ExplicitVolumeChange" {
+                    print(userInfo[AnyHashable("AVSystemController_AudioVolumeNotificationParameter")])
+                    if volumeValue > userInfo[AnyHashable("AVSystemController_AudioVolumeNotificationParameter")] as! Float{
+                        print("volume down")
+                    }
+                    else if volumeValue < userInfo[AnyHashable("AVSystemController_AudioVolumeNotificationParameter")] as! Float{
+                        print("volume up")
+                    }
+                    else if volumeValue == userInfo[AnyHashable("AVSystemController_AudioVolumeNotificationParameter")] as! Float && volumeValue == 1{
+                        print("volume max")
+                    }
+                    else if volumeValue == userInfo[AnyHashable("AVSystemController_AudioVolumeNotificationParameter")] as! Float && volumeValue == 0{
+                        print("volume min")
+                    }
+                    volumeValue = userInfo[AnyHashable("AVSystemController_AudioVolumeNotificationParameter")] as! Float
+                }
+            }
+        }
     }
 }
 
